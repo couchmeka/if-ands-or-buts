2d2f43163f1468c1bc827d5ec916ff35743895f0
couchmeka <meka.couch2@gmail.com>
2022-09-15 18:16:17 -0400

    new commit

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -7,25 +7,25 @@ let player2 = String(prompt("Choose rock, paper, or scissors: "));
 if(player1 === "rock" && player2 === "scissors")
 {
     console.log("Player1 WINS!");
-} if (player1 === "scissors" && player2 === "rock")
+} else if (player1 === "scissors" && player2 === "rock")
 {
     console.log("Player2 WINS!");
-} if (player1 === "rock" && player2 === "scissors")
+} else if (player1 === "rock" && player2 === "scissors")
 {
     console.log("Player1 WINS!");
-} if (player1 === "paper" && player2 === "scissors")
+} else if (player1 === "paper" && player2 === "scissors")
 {
     console.log("Player2 WINS!");
-} if (player1 === "scissors" && player2 === "paper")
+} else if (player1 === "scissors" && player2 === "paper")
 {
     console.log("Player1 WINS!");
-} if (player1 === "rock" && player2 === "paper"){
+} else if (player1 === "rock" && player2 === "paper"){
     console.log("Player2 WINS!")
-} if (player1 === "paper" && player2 === "rock"){
+} else if (player1 === "paper" && player2 === "rock"){
     console.log("Player1 WINS!");
-} if (player1 === player2){
+} else if (player1 === player2){
     console.log("draw");
-} if (player1 !== String || player2 !== String){
+} else if (player1 !== String || player2 !== String){
     console.log("error")
 } else {
     console.log("error")
