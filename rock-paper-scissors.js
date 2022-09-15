5c07124de8c21c68060136976ad43b0e6c409ffd
couchmeka <meka.couch2@gmail.com>
2022-09-15 18:11:36 -0400

    new commit

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -7,21 +7,22 @@ let player2 = String(prompt("Choose rock, paper, or scissors: "));
 if(player1 === "rock" && player2 === "scissors")
 {
     console.log("Player1 WINS!");
-} else if (player1 === "scissors" && player2 === "rock")
+} if (player1 === "scissors" && player2 === "rock")
 {
     console.log("Player2 WINS!");
-} else if (player1 === "rock" && player2 === "scissors")
+} if (player1 === "rock" && player2 === "scissors")
 {
     console.log("Player1 WINS!");
-} else if (player1 === "paper" && player2 === "scissors")
+} if (player1 === "paper" && player2 === "scissors")
 {
     console.log("Player2 WINS!");
-} 
- else if (player1 === "scissors" && player2 === "paper")
+} if (player1 === "scissors" && player2 === "paper")
 {
     console.log("Player1 WINS!");
-} else if (player1 === player2){
+} if (player1 === "rock" && player2 === "paper"){
+    console.log("Player2 WINS!")
+} if (player1 === "paper" && player2 === "rock"){
+    console.log("Player1 WINS!");
+} if (player1 === player2){
     console.log("draw");
-} else {
-    console.log("error");
-}
+} 
