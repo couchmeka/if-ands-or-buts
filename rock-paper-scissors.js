0e26c17ab196dfafc19259f5099bbaf8f5712010
couchmeka <meka.couch2@gmail.com>
2022-09-15 15:56:57 -0400

    semicolon

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -6,20 +6,20 @@ let player2 = String(prompt("Choose rock, paper, or scissors: "));
 
 if(player1 === "rock" && player2 === "scissors")
 {
-    console.log("Player1 WINS!")
+    console.log("Player1 WINS!");
 } else if (player1 === "scissors" && player2 === "rock")
 {
-    console.log("Player2 WINS!")
+    console.log("Player2 WINS!");
 } else if (player1 === "rock" && player2 === "scissors")
 {
-    console.log("Player1 WINS!")
+    console.log("Player1 WINS!");
 } else if(player1 === "paper" && player2 === "scissors")
 {
-    console.log("Player2 WINS!")
+    console.log("Player2 WINS!");
 } 
  else if (player1 === "scissors" && player2 === "paper")
 {
     console.log("Player1 WINS!")
 } else if (player1 === player2){
-    console.log("draw")
+    console.log("draw");
 }
