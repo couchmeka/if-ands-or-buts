76e5f80be4d4bb6779062c3e4ef53343ca2343e3
CouchMeka <meka.daniels1@gmail.com>
2022-09-19 11:03:14 -0400

    pin code

--- lock-box.js
+++ lock-box.js
@@ -1,9 +1,9 @@
 const prompt = require('prompt-sync')({sigint: true});
-
+let pin =1234;
 let lockBox = Number(prompt("Pick a four digit pin code: "));
 
 
-if (lockBox === 1234){
+if (lockBox === pin){
     console.log("success")
 } else { 
     console.log("failure")
--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -2,7 +2,8 @@ const prompt = require('prompt-sync')({sigint: true});
 
 let player1 = String(prompt("Choose rock, paper, or scissors: "));
 let player2 = String(prompt("Choose rock, paper, or scissors: "));
-
+player1 = player1.toLowerCase();
+player2 = player2.toLowerCase();
 
 if(player1 === "rock" && player2 === "scissors")
 {
@@ -25,7 +26,13 @@ if(player1 === "rock" && player2 === "scissors")
 } else if (player1 === "paper" && player2 === "rock")
 {
     console.log("Player1 WINS!");
-} else if (player1 === player2)
+} else if (player1 === "paper" && player2 === "paper")
+{
+    console.log("draw");
+} else if (player1 === "rock" && player2 === "rock")
+{
+    console.log("draw");
+} else if (player1 === "scissors" && player2 === "scissors")
 {
     console.log("draw");
 } else if (player1 !== String || player2 !== String)
